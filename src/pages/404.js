90437be457ff2f82a324eddb90651013eb28151f
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2018-04-11 15:10:02 +0530

    404 Not Found Page

---
+++ src/pages/404.js
@@ -0,0 +1,30 @@
+/**
+ * Created by vaibhav on 11/4/18
+ */
+import React from 'react'
+
+const NotFoundPage = () => (
+  <div>
+    <section className="hero is-primary is-bold is-medium">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+              <div className="section has-text-centered">
+                <h1 className="title">
+                  404: NOT FOUND
+                </h1>
+                <h2 className="subtitle">
+                  You just hit a route that doesn&#39;t exist... the
+                  sadness.
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+)
+
+export default NotFoundPage
