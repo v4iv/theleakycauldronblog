e21425a5e4c61a277ebfa73667e2b2966d834cc6
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2018-04-16 13:08:03 +0530

    Six Articles Per Page

--- gatsby-node.js
+++ gatsby-node.js
@@ -40,7 +40,7 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
       edges: result.data.allMarkdownRemark.edges,
       createPage: createPage,
       pageTemplate: 'src/templates/index.js',
-      pageLength: 5, // This is optional and defaults to 10 if not used
+      pageLength: 6, // This is optional and defaults to 10 if not used
       pathPrefix: '', // This is optional and defaults to an empty string if not used
       context: {}, // This is optional and defaults to an empty object if not used
     })
