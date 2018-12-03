8b633bdfa1738322127c289758ee33de0ba22e16
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2018-12-04 01:10:49 +0530

    Add Powered by to the Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,8 @@ const Footer = ({ config }) => {
     <footer className='footer'>
       <div className='container'>
         <div className='content has-text-centered'>
-          <p>{config.copyright} Powered by <a href='https://www.gatsbyjs.org' rel='noreferrer nofollow'>Gatsby v2</a></p>
+          <p>{config.copyright}</p>
+          <p>Powered by <a href='https://www.gatsbyjs.org' rel='noreferrer nofollow' target='_blank'>Gatsby</a> and <a href='https://www.netlifycms.org' rel='noreferrer nofollow' target='_blank'>Netlify CMS</a></p>
         </div>
       </div>
     </footer>
