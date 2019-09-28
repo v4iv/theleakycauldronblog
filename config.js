8f538c8f7ac8a0a2c0c14def21e0b8dfc2b3e271
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2019-09-28 14:47:04 +0530

    feat: updated netlify.toml

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
   siteUrl: 'https://theleakycauldronblog.com', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'A Brew of Awesomeness with a Pinch of Magic...', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'The Leaky Cauldron Blog is the personal blog of Vaibhav Sharma. A Brew of Awesomeness with a Pinch of Magic...', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   googleTagManagerID: process.env.GTM_ID || '', // GTM tracking ID.
   disqusShortname: 'https-theleakycauldronblog-com', // Disqus shortname.
