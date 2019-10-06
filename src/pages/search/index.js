abb5e14e6c28215bccb99b8772c51e040d1cc264
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2019-10-07 00:16:09 +0530

    feat: a11y improvements

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql, StaticQuery } from 'gatsby'
+import Helmet from 'react-helmet'
 import 'tachyons-sass/tachyons.scss'
 import SearchBox from '../../components/SearchBox'
 
@@ -12,10 +13,28 @@ const SearchPage = () => (
                 }
             }
         `}
-    render={data => (
-      <section className='vh-100 avenir'>
-        <SearchBox searchIndex={data.siteSearchIndex.index} />
-      </section>)}
+    render={data =>
+      <>
+        <Helmet
+          htmlAttributes={{
+            lang: `en`,
+          }}
+          title='Search | The Leaky Cauldron Blog'
+          meta={[
+            {
+              name: `description`,
+              content: `Search The Leaky Cauldron Blog!`,
+            },
+            {
+              name: `viewport`,
+              content: `width=device-width, initial-scale=1`,
+            },
+          ]}
+        />
+        <section className='vh-100 avenir'>
+          <SearchBox searchIndex={data.siteSearchIndex.index} />
+        </section>
+      </>}
   />
 )
 
