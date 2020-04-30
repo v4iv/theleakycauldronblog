ec8f3f009e5053361bab1d91b95331bac8a04bae
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2020-04-30 20:35:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const config = {
   siteDescription: 'The Leaky Cauldron Blog is the personal blog of Vaibhav Sharma. A Brew of Awesomeness with a Pinch of Magic...', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   googleTagManagerID: process.env.GTM_ID || '', // GTM tracking ID.
-  disqusShortname: 'https-theleakycauldronblog-com', // Disqus shortname.
+  disqusShortname: process.env.DISQUS_SHORTNAME || '', // Disqus shortname.
   userName: 'Vaibhav Sharma',
   userTwitter: 'vaibhaved',
   userLocation: 'Delhi NCR, India',
