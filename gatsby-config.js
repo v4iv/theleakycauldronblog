3aa76ff1af40aa158987070e144ed90d04a5becb
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2020-10-16 13:31:35 +0530

    fix: network first for serviceworker

--- gatsby-config.js
+++ gatsby-config.js
@@ -164,6 +164,36 @@ module.exports = {
     {
       resolve: `gatsby-plugin-offline`,
       options: {
+        workboxConfig: {
+          runtimeCaching: [
+            {
+              urlPattern: /(\.js$|\.css$|static\/)/,
+              handler: `CacheFirst`,
+            },
+            {
+              urlPattern: /^https?:.*\/page-data\/.*\/(page-data|app-data)\.json$/,
+              handler: `NetworkFirst`,
+              options: {
+                networkTimeoutSeconds: 1,
+              },
+            },
+            {
+              urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+              handler: `StaleWhileRevalidate`,
+            },
+            {
+              urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
+              handler: `StaleWhileRevalidate`,
+            },
+            {
+              urlPattern: /\/$/,
+              handler: `NetworkFirst`,
+              options: {
+                networkTimeoutSeconds: 1,
+              },
+            },
+          ],
+        },
         precachePages: [`/blog/*`, `/about`],
       },
     },
