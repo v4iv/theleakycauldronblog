27b5d9bdc4aea4c2e7382d39a19c2bb22373339e
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2020-12-26 18:39:50 +0530

    feat: Newsletter Form

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Socials from '../Socials'
+import NewsletterForm from '../NewsletterForm'
 
 const Footer = (props) => {
   const { copyright, siteRss } = props
 
   return <footer className='pa4 pa5-l black-70 bt b--black-10 mw8 center pv5'>
+    <section className="center cf mb4">
+      <div className="mb4 mb0-ns fl w-100">
+        <p className="f4 fw6 mb2 f6 mt0 avenir">
+          Sign up for our newsletter.
+        </p>
+        <NewsletterForm/>
+      </div>
+    </section>
     <div className='dt dt--fixed w-100'>
       <div className='dn dtc-ns v-mid'>
         <p className='f7 black-70 dib pr3 mb3'>
-          {copyright} Powered by <a className='link black hover-purple' href='https://www.gatsbyjs.org' target='_blank' rel='noreferrer nofollow noopener'>Gatsby</a>
+          {copyright} Powered by <a className='link black hover-purple' href='https://www.gatsbyjs.com' target='_blank' rel='noreferrer nofollow noopener'>Gatsby</a>
         </p>
       </div>
       <Socials siteRss={siteRss} />
