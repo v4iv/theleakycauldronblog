c3eb35fb80c314764193cc3d7343569f8e61c643
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2023-07-26 14:45:47 +0530

    feat: partytown debug

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -6,6 +6,7 @@ import {ThemeProvider} from './src/components/ui/theme-context'
 export const onRenderBody: GatsbySSR['onRenderBody'] = ({
   setHtmlAttributes,
   setHeadComponents,
+  setPreBodyComponents,
 }) => {
   setHtmlAttributes({
     lang: 'en',
@@ -16,45 +17,59 @@ export const onRenderBody: GatsbySSR['onRenderBody'] = ({
       key="set-theme"
       dangerouslySetInnerHTML={{
         __html: `
-            function getThemePreference() {
-              var storedColorPreference = localStorage.getItem('theme')
-            
-              if (typeof storedColorPreference === 'string') return storedColorPreference
-            
-              var prefersDarkMode = window.matchMedia(
-                '(prefers-color-scheme: dark)',
-              ).matches
-            
-              return prefersDarkMode ? 'dark' : 'light'
-            }
+                    function getThemePreference() {
+                      var storedColorPreference = localStorage.getItem('theme')
+                    
+                      if (typeof storedColorPreference === 'string') return storedColorPreference
+                    
+                      var prefersDarkMode = window.matchMedia(
+                        '(prefers-color-scheme: dark)',
+                      ).matches
+                    
+                      return prefersDarkMode ? 'dark' : 'light'
+                    }
 
-            var theme = getThemePreference()
+                    var theme = getThemePreference()
 
-            document.body.classList.add(theme)
-        `,
+                    document.body.classList.add(theme)
+                `,
       }}
     />,
     <script
       key="partytown-vanilla-config"
       dangerouslySetInnerHTML={{
         __html: `partytown = {
-            resolveUrl(url, location) {
-                if (url.hostname.includes('google-analytics')) {
-                  // Use a secure connection
-                  if (url?.protocol === 'http:') {
-                    url = new URL(url.href.replace('http', 'https'))
+                    resolveUrl(url, location) {
+                      if (url.hostname.includes('google-analytics')) {
+                        // Use a secure connection
+                        if (url?.protocol === 'http:') {
+                          url = new URL(url.href.replace('http', 'https'))
+                        }
+
+                        // Point to our proxied URL
+                        const proxyUrl = new URL(location.origin + '/__third-party-proxy')
+                        proxyUrl.searchParams.append('url', url)
+
+                        return proxyUrl
+                      }
+
+                      return url
+                    },
+                    debug: true
                   }
+                `,
+      }}
+    />,
+  ])
 
-                  // Point to our proxied URL
-                  const proxyUrl = new URL(location.origin + '/__third-party-proxy')
-                  proxyUrl.searchParams.append('url', url)
-
-                  return proxyUrl
-                }
-
-                return url
-            }
-        }`,
+  setPreBodyComponents([
+    <noscript
+      key="gtm"
+      dangerouslySetInnerHTML={{
+        __html: `
+                  <iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GATSBY_GTM_ID}" height="0" width="0"
+                      style="display:none;visibility:hidden"></iframe>
+                `,
       }}
     />,
   ])
