7d159eda2f382dee5f7081b2b407ec260fe97888
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2023-08-01 15:13:07 +0530

    feat: party-town debug disabled

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -17,12 +17,12 @@ const siteMetadata = {
 
 const config: GatsbyConfig = {
   siteMetadata,
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   partytownProxiedURLs: [
     `https://www.googletagmanager.com/gtag/js?id=${process.env.GATSBY_GTAG}`,
   ],
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
     {
