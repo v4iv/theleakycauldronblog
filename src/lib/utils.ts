f532cee67f84748ace79ae56c9f50b89b4445acf
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2023-08-16 14:56:29 +0530

    feat: themes (#326)
    * feat: themes
    
    * feat: i18n
    
    * feat: clean-up

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,10 +11,10 @@ export function encode(data: {[key: string]: any}) {
     .join('&')
 }
 
-export function getThemePreference() {
-  if (typeof window === 'undefined') return 'light'
+export function getThemePreference(storageKey: string) {
+  if (typeof window === 'undefined') return 'system'
 
-  const storedTheme = localStorage.getItem('theme')
+  const storedTheme = localStorage.getItem(storageKey)
 
   if ('system' === storedTheme || (!storedTheme && true)) {
     const query = '(prefers-color-scheme: dark)',
@@ -26,5 +26,5 @@ export function getThemePreference() {
     }
   }
 
-  return 'light'
+  return 'system'
 }
