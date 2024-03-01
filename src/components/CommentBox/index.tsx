49ad9fcfc1e07c7a5eb3c2331f083cf59d14b514
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2024-03-01 18:22:08 +0530

    feat: Use Zaraz to load Disqus

--- src/components/CommentBox/index.tsx
+++ src/components/CommentBox/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Disqus} from 'gatsby-plugin-disqus'
+import {Script} from 'gatsby'
 interface CommentBoxProps {
   id: string
   slug: string
@@ -7,19 +7,21 @@ interface CommentBoxProps {
   siteURL: string
 }
 
-function CommentBox({id, slug, title, siteURL}: CommentBoxProps) {
+function CommentBox({id, slug, siteURL}: CommentBoxProps) {
   const url = siteURL + slug
 
-  const disqusConfig = {
-    url: url,
-    title: title,
-    identifier: id,
-  }
-
   return (
-    <div className="p-3 md:px-0 md:py-5">
-      <Disqus config={disqusConfig} />
-    </div>
+    <>
+      <div id="disqus_thread" className="p-3 md:px-0 md:py-5" />
+      <Script id="disqus-config-loader">
+        {`
+          var disqus_config = function () {
+            this.page.url = ${url};  // Replace PAGE_URL with your page's canonical URL variable
+            this.page.identifier = ${id}; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+          };
+        `}
+      </Script>
+    </>
   )
 }
 
