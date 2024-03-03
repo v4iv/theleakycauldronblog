1514cddc8f50039f3e1db3e7a7658d1156c9bc3c
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2024-03-03 20:00:18 +0530

    feat: Gatsby Script ID

--- src/components/CommentBox/index.tsx
+++ src/components/CommentBox/index.tsx
@@ -14,17 +14,10 @@ function CommentBox({id, title, slug, siteURL}: CommentBoxProps) {
   // This is just to load the disqus config for the page, Disqus itself is being loaded using Cloudflare Zaraz
   return (
     <>
-      <div id="disqus_thread" className="p-3 md:px-0 md:py-5">
-        <noscript>
-          Please enable JavaScript to view the{' '}
-          <a href="https://disqus.com/?ref_noscript">
-            comments powered by Disqus.
-          </a>
-        </noscript>
-      </div>
+      <div id="disqus_thread" className="p-3 md:px-0 md:py-5" />
 
       <Script
-        id="disqus-config-loader"
+        id={id}
         strategy={ScriptStrategy.postHydrate}
         dangerouslySetInnerHTML={{
           __html: `var disqus_config = function () {
@@ -34,6 +27,15 @@ function CommentBox({id, title, slug, siteURL}: CommentBoxProps) {
           };`,
         }}
       />
+
+      <noscript>
+        <div className="p-3 md:px-0 md:py-5">
+          Please enable JavaScript to view the{' '}
+          <a href="https://disqus.com/?ref_noscript">
+            comments powered by Disqus.
+          </a>
+        </div>
+      </noscript>
     </>
   )
 }
