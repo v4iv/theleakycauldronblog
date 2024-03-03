20a867760a1d0575928e666ae4dc298455a6ec23
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2024-03-03 19:49:59 +0530

    feat: disqus noscript

--- src/components/CommentBox/index.tsx
+++ src/components/CommentBox/index.tsx
@@ -14,13 +14,24 @@ function CommentBox({id, title, slug, siteURL}: CommentBoxProps) {
   // This is just to load the disqus config for the page, Disqus itself is being loaded using Cloudflare Zaraz
   return (
     <>
-      <div id="disqus_thread" className="p-3 md:px-0 md:py-5" />
+      <div id="disqus_thread" className="p-3 md:px-0 md:py-5">
+        <noscript>
+          Please enable JavaScript to view the{' '}
+          <a href="https://disqus.com/?ref_noscript">
+            comments powered by Disqus.
+          </a>
+        </noscript>
+      </div>
 
       <Script
         id="disqus-config-loader"
         strategy={ScriptStrategy.postHydrate}
         dangerouslySetInnerHTML={{
-          __html: `var disqus_config = function () {this.page.url = "${url}"; this.page.title = "${title}"; this.page.identifier = "${id}"; };`,
+          __html: `var disqus_config = function () {
+            this.page.url = "${url}";
+            this.page.identifier = "${id}"; 
+            this.page.title = "${title}";
+          };`,
         }}
       />
     </>
