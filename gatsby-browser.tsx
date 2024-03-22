296b5b1faf556fc92db5ca1c487509e38ad91723
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2024-03-22 12:33:31 +0530

    Use Dark Mode (#346)
    * feat: use-dark-mode hook
    
    * feat: remove command palette

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -2,14 +2,9 @@ import React from 'react'
 import {GatsbyBrowser} from 'gatsby'
 import './src/styles/globals.css'
 import {TooltipProvider} from './src/components/ui/tooltip'
-import {ThemeProvider} from './src/components/ui/theme-provider'
 
 export const wrapRootElement: GatsbyBrowser['wrapRootElement'] = ({
   element,
 }) => {
-  return (
-    <ThemeProvider>
-      <TooltipProvider>{element}</TooltipProvider>
-    </ThemeProvider>
-  )
+  return <TooltipProvider>{element}</TooltipProvider>
 }
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import React, {lazy, Suspense, useState, useEffect} from 'react'
+import React from 'react'
 import {Link} from 'gatsby'
 import {
   Equal,
@@ -10,9 +10,11 @@ import {
   Sun,
   Hash,
 } from 'lucide-react'
+import useDarkMode from 'use-dark-mode'
 import {StaticImage} from 'gatsby-plugin-image'
 import {useTranslation} from 'gatsby-plugin-react-i18next'
 
+import {darkModeConfig} from '@/lib/utils'
 import {
   Sheet,
   SheetContent,
@@ -29,248 +31,212 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import {Button} from '@/components/ui/button'
-import {useTheme} from '@/components/ui/theme-provider'
 import {Avatar, AvatarFallback, AvatarImage} from '@/components/ui/avatar'
 import {Tooltip, TooltipContent, TooltipTrigger} from '@/components/ui/tooltip'
 
-const CommandPalette = lazy(() => import('@/components/CommandPalette'))
-
 function NavBar() {
   const {t} = useTranslation('common')
-  const [open, setOpen] = useState(false)
-  const {setTheme} = useTheme()
-
-  useEffect(() => {
-    const down = (e: KeyboardEvent) => {
-      if (e.key === 'k' && e.metaKey) {
-        setOpen((open) => !open)
-      }
-    }
-    document.addEventListener('keydown', down)
-    return () => document.removeEventListener('keydown', down)
-  }, [])
+  const darkMode = useDarkMode(false, darkModeConfig)
 
   return (
-    <>
-      <nav className="mx-auto w-full max-w-screen-md font-mono">
-        <div className="p-3 md:px-0 md:py-5">
-          <div className="flex">
-            <div className="flex grow">
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Link aria-label={t('site-short-name')} to="/">
-                    <Avatar className="mr-2 flex md:hidden">
-                      <AvatarImage
-                        src="/icon-192-maskable.png"
+    <nav className="mx-auto w-full max-w-screen-md font-mono">
+      <div className="p-3 md:px-0 md:py-5">
+        <div className="flex">
+          <div className="flex grow">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link aria-label={t('site-short-name')} to="/">
+                  <Avatar className="mr-2 flex md:hidden">
+                    <AvatarImage
+                      src="/icon-192-maskable.png"
+                      alt="the-leaky-cauldron-blog"
+                      asChild
+                    >
+                      <StaticImage
+                        src="../../assets/avatar.png"
                         alt="the-leaky-cauldron-blog"
-                        asChild
-                      >
-                        <StaticImage
-                          src="../../assets/avatar.png"
-                          alt="the-leaky-cauldron-blog"
-                        />
-                      </AvatarImage>
-                      <AvatarFallback>{t('site-short-name')}</AvatarFallback>
-                    </Avatar>
+                      />
+                    </AvatarImage>
+                    <AvatarFallback>{t('site-short-name')}</AvatarFallback>
+                  </Avatar>
+                </Link>
+              </TooltipTrigger>
+
+              <TooltipContent>
+                <p>{t('home')}</p>
+              </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <h3 className="scroll-m-20 text-2xl font-extrabold leading-relaxed tracking-wider">
+                  <Link
+                    className="hidden hover:text-muted-foreground md:flex"
+                    to="/"
+                  >
+                    {t('site-name')}
                   </Link>
+
+                  <Link
+                    className="flex transition-colors duration-100 hover:text-gray-500 md:hidden"
+                    to="/"
+                  >
+                    {t('site-short-name')}
+                  </Link>
+                </h3>
+              </TooltipTrigger>
+
+              <TooltipContent>
+                <p>{t('home')}</p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+
+          <div className="flex space-x-2">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  aria-label={t('search')}
+                  asChild
+                >
+                  <Link to="/search">
+                    <Search className="size-4 text-aquamarine-500" />
+                  </Link>
+                </Button>
+              </TooltipTrigger>
+
+              <TooltipContent>
+                <p>{t('search')}</p>
+              </TooltipContent>
+            </Tooltip>
+
+            <Sheet>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <SheetTrigger asChild>
+                    <Button
+                      variant="outline"
+                      size="icon"
+                      aria-label={t('menu')}
+                    >
+                      <Equal className="size-6 text-aquamarine-500" />
+                    </Button>
+                  </SheetTrigger>
                 </TooltipTrigger>
 
                 <TooltipContent>
-                  <p>{t('home')}</p>
+                  <p>{t('menu')}</p>
                 </TooltipContent>
               </Tooltip>
 
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <h3 className="scroll-m-20 text-2xl font-extrabold leading-relaxed tracking-wider">
-                    <Link
-                      className="hidden hover:text-muted-foreground md:flex"
-                      to="/"
-                    >
-                      {t('site-name')}
-                    </Link>
+              <SheetContent className="flex flex-col font-mono">
+                <SheetHeader>
+                  <SheetTitle className="text-aquablue-500">
+                    {t('menu')}
+                  </SheetTitle>
+                </SheetHeader>
 
-                    <Link
-                      className="flex transition-colors duration-100 hover:text-gray-500 md:hidden"
-                      to="/"
-                    >
-                      {t('site-short-name')}
-                    </Link>
-                  </h3>
-                </TooltipTrigger>
-
-                <TooltipContent>
-                  <p>{t('home')}</p>
-                </TooltipContent>
-              </Tooltip>
-            </div>
-
-            <div className="flex space-x-2">
-              <Tooltip>
-                <TooltipTrigger asChild>
+                <SheetDescription className="flex grow flex-col gap-y-5">
                   <Button
-                    variant="outline"
-                    size="icon"
+                    variant="ghost"
+                    className="justify-start"
+                    aria-label={t('home')}
+                    asChild
+                  >
+                    <Link className="flex" to="/">
+                      <Home className="mr-3 size-4 text-aquamarine-500" />
+                      {t('home')}
+                    </Link>
+                  </Button>
+
+                  <Button
+                    variant="ghost"
+                    className="justify-start"
                     aria-label={t('search')}
                     asChild
                   >
-                    <Link to="/search">
-                      <Search className="size-4 text-aquamarine-500" />
+                    <Link className="flex" to="/search/">
+                      <Search className="mr-2 size-4 text-aquamarine-500" />
+                      {t('search')}
                     </Link>
                   </Button>
-                </TooltipTrigger>
 
-                <TooltipContent>
-                  <p>{t('search')}</p>
-                </TooltipContent>
-              </Tooltip>
+                  <Button
+                    variant="ghost"
+                    className="justify-start"
+                    aria-label={t('tags')}
+                    asChild
+                  >
+                    <Link className="flex" to="/tags/">
+                      <Hash className="mr-2 size-4 text-aquamarine-500" />
+                      {t('tags')}
+                    </Link>
+                  </Button>
 
-              <Sheet>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <SheetTrigger asChild>
-                      <Button
-                        variant="outline"
-                        size="icon"
-                        aria-label={t('menu')}
-                      >
-                        <Equal className="size-6 text-aquamarine-500" />
+                  <Button
+                    variant="ghost"
+                    className="justify-start"
+                    aria-label={t('about')}
+                    asChild
+                  >
+                    <Link className="flex" to="/about/">
+                      <MessageCircle className="mr-2 size-4 text-aquamarine-500" />
+                      {t('about')}
+                    </Link>
+                  </Button>
+
+                  <Button
+                    variant="ghost"
+                    className="justify-start"
+                    aria-label={t('contact')}
+                    asChild
+                  >
+                    <Link className="flex" to="/contact/">
+                      <AtSign className="mr-2 size-4 text-aquamarine-500" />
+                      {t('contact')}
+                    </Link>
+                  </Button>
+                </SheetDescription>
+
+                <SheetFooter className="flex">
+                  <div className="grow" />
+
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="ghost" size="icon">
+                        <Sun className="size-[1.2rem] rotate-0 scale-100 text-aquamarine-500 transition-all dark:-rotate-90 dark:scale-0" />
+
+                        <Moon className="absolute size-[1.2rem] rotate-90 scale-0 text-aquamarine-500 transition-all dark:rotate-0 dark:scale-100" />
+
+                        <span className="sr-only">{t('toggle-theme')}</span>
                       </Button>
-                    </SheetTrigger>
-                  </TooltipTrigger>
+                    </DropdownMenuTrigger>
 
-                  <TooltipContent>
-                    <p>{t('menu')}</p>
-                  </TooltipContent>
-                </Tooltip>
+                    <DropdownMenuContent side="top" align="end">
+                      <DropdownMenuItem
+                        className="font-mono"
+                        onClick={darkMode.disable}
+                      >
+                        {t('light')}
+                      </DropdownMenuItem>
 
-                <SheetContent className="flex flex-col font-mono">
-                  <SheetHeader>
-                    <SheetTitle className="text-aquablue-500">
-                      {t('menu')}
-                    </SheetTitle>
-                  </SheetHeader>
-
-                  <SheetDescription className="flex grow flex-col gap-y-5">
-                    <Button
-                      variant="ghost"
-                      className="justify-start"
-                      aria-label={t('home')}
-                      asChild
-                    >
-                      <Link className="flex" to="/">
-                        <Home className="mr-3 size-4 text-aquamarine-500" />
-                        {t('home')}
-                      </Link>
-                    </Button>
-
-                    <Button
-                      variant="ghost"
-                      className="justify-start"
-                      aria-label={t('search')}
-                      asChild
-                    >
-                      <Link className="flex" to="/search/">
-                        <Search className="mr-2 size-4 text-aquamarine-500" />
-                        {t('search')}
-                      </Link>
-                    </Button>
-
-                    <Button
-                      variant="ghost"
-                      className="justify-start"
-                      aria-label={t('tags')}
-                      asChild
-                    >
-                      <Link className="flex" to="/tags/">
-                        <Hash className="mr-2 size-4 text-aquamarine-500" />
-                        {t('tags')}
-                      </Link>
-                    </Button>
-
-                    <Button
-                      variant="ghost"
-                      className="justify-start"
-                      aria-label={t('about')}
-                      asChild
-                    >
-                      <Link className="flex" to="/about/">
-                        <MessageCircle className="mr-2 size-4 text-aquamarine-500" />
-                        {t('about')}
-                      </Link>
-                    </Button>
-
-                    <Button
-                      variant="ghost"
-                      className="justify-start"
-                      aria-label={t('contact')}
-                      asChild
-                    >
-                      <Link className="flex" to="/contact/">
-                        <AtSign className="mr-2 size-4 text-aquamarine-500" />
-                        {t('contact')}
-                      </Link>
-                    </Button>
-                  </SheetDescription>
-
-                  <SheetFooter className="flex">
-                    <p className="hidden font-mono text-[10px] text-muted-foreground md:flex md:items-center">
-                      {t('quick-search')}&nbsp;&raquo;&nbsp;
-                      <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-                        <span className="text-xs">&#8984;</span>K
-                      </kbd>
-                    </p>
-
-                    <div className="grow" />
-
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" size="icon">
-                          <Sun className="size-[1.2rem] rotate-0 scale-100 text-aquamarine-500 transition-all dark:-rotate-90 dark:scale-0" />
-
-                          <Moon className="absolute size-[1.2rem] rotate-90 scale-0 text-aquamarine-500 transition-all dark:rotate-0 dark:scale-100" />
-
-                          <span className="sr-only">{t('toggle-theme')}</span>
-                        </Button>
-                      </DropdownMenuTrigger>
-
-                      <DropdownMenuContent side="top" align="end">
-                        <DropdownMenuItem
-                          className="font-mono"
-                          onClick={() => setTheme('light')}
-                        >
-                          {t('light')}
-                        </DropdownMenuItem>
-
-                        <DropdownMenuItem
-                          className="font-mono"
-                          onClick={() => setTheme('dark')}
-                        >
-                          {t('dark')}
-                        </DropdownMenuItem>
-
-                        <DropdownMenuItem
-                          className="font-mono"
-                          onClick={() => setTheme('system')}
-                        >
-                          {t('system')}
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </SheetFooter>
-                </SheetContent>
-              </Sheet>
-            </div>
+                      <DropdownMenuItem
+                        className="font-mono"
+                        onClick={darkMode.enable}
+                      >
+                        {t('dark')}
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </SheetFooter>
+              </SheetContent>
+            </Sheet>
           </div>
         </div>
-      </nav>
-
-      {open && (
-        <Suspense>
-          <CommandPalette open={open} setOpen={setOpen} />
-        </Suspense>
-      )}
-    </>
+      </div>
+    </nav>
   )
 }
 
