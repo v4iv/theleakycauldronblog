1e4a50c4352980c9ecec5cfe08264afa661cb161
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2025-01-15 12:22:50 +0530

    fix: remove manual init of decap cms

--- src/cms/cms.ts
+++ src/cms/cms.ts
@@ -5,7 +5,6 @@ import AboutPagePreview from './AboutPagePreview'
 import ContactPagePreview from './ContactPagePreview'
 import ArticlePagePreview from './ArticlePagePreview'
 
-CMS.init()
 CMS.registerPreviewTemplate('blog', ArticlePagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('contact', ContactPagePreview)
