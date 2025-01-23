e61df8c493c1b80121e30c15d3e5bf2a489ef7dd
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2025-01-23 13:29:37 +0530

    :tada: easter egg :tada: (#369)

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,86 +10,91 @@ function Footer() {
   const {t} = useTranslation('common')
 
   return (
-    <footer className="mx-auto w-full max-w-screen-md font-mono">
-      <div className="flex flex-col items-center px-3 py-5 text-center md:flex-row md:px-0 md:text-left">
-        <div className="grow">
-          <TypographyMuted className="font-extralight">
-            {t('footer.copyright', {
-              year: 25,
-              // @TODO: figure out the render difference between server and client side
-              // year: new Date().getFullYear().toString().slice(2),
-            })}
-          </TypographyMuted>
-        </div>
+    <>
+      <footer className="mx-auto w-full max-w-screen-md font-mono">
+        <div className="flex flex-col items-center px-3 py-5 text-center md:flex-row md:px-0 md:text-left">
+          <div className="grow">
+            <TypographyMuted className="font-extralight">
+              {t('footer.copyright', {
+                year: 25,
+                // @TODO: figure out the render difference between server and client side
+                // year: new Date().getFullYear().toString().slice(2),
+              })}
+            </TypographyMuted>
+          </div>
 
-        <div className="mt-3 flex space-x-2 md:mt-0">
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                aria-label={t('footer.github')}
-                asChild
-              >
-                <a
-                  target="_blank"
-                  rel="noreferrer nofollow noopener"
-                  href="https://github.com/v4iv"
+          <div className="mt-3 flex space-x-2 md:mt-0">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  aria-label={t('footer.github')}
+                  asChild
                 >
-                  <Github className="size-4" />
-                </a>
-              </Button>
-            </TooltipTrigger>
+                  <a
+                    target="_blank"
+                    rel="noreferrer nofollow noopener"
+                    href="https://github.com/v4iv"
+                  >
+                    <Github className="size-4" />
+                  </a>
+                </Button>
+              </TooltipTrigger>
 
-            <TooltipContent>
-              <p>{t('footer.github')}</p>
-            </TooltipContent>
-          </Tooltip>
+              <TooltipContent>
+                <p>{t('footer.github')}</p>
+              </TooltipContent>
+            </Tooltip>
 
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                aria-label={t('footer.twitter')}
-                asChild
-              >
-                <a
-                  target="_blank"
-                  rel="noreferrer nofollow noopener"
-                  href="https://twitter.com/waybove"
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  aria-label={t('footer.twitter')}
+                  asChild
                 >
-                  <Twitter className="size-4 text-[#1da1f2]" />
-                </a>
-              </Button>
-            </TooltipTrigger>
+                  <a
+                    target="_blank"
+                    rel="noreferrer nofollow noopener"
+                    href="https://twitter.com/waybove"
+                  >
+                    <Twitter className="size-4 text-[#1da1f2]" />
+                  </a>
+                </Button>
+              </TooltipTrigger>
 
-            <TooltipContent>
-              <p>{t('footer.twitter')}</p>
-            </TooltipContent>
-          </Tooltip>
+              <TooltipContent>
+                <p>{t('footer.twitter')}</p>
+              </TooltipContent>
+            </Tooltip>
 
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                aria-label={t('footer.rss')}
-                asChild
-              >
-                <a target="_blank" href="/rss.xml" rel="noopener noreferrer">
-                  <Rss className="size-4 text-[#ee802f]" />
-                </a>
-              </Button>
-            </TooltipTrigger>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  aria-label={t('footer.rss')}
+                  asChild
+                >
+                  <a target="_blank" href="/rss.xml" rel="noopener noreferrer">
+                    <Rss className="size-4 text-[#ee802f]" />
+                  </a>
+                </Button>
+              </TooltipTrigger>
 
-            <TooltipContent>
-              <p>{t('footer.rss')}</p>
-            </TooltipContent>
-          </Tooltip>
+              <TooltipContent>
+                <p>{t('footer.rss')}</p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
         </div>
+      </footer>
+      <div className="flex justify-start">
+        <span className="easter-egg invert"></span>
       </div>
-    </footer>
+    </>
   )
 }
 
