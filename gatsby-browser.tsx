1c106285a00955214500e21f9bd326253f43a4ed
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2025-01-23 13:28:34 +0530

    :tada: easter egg :tada:

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {GatsbyBrowser} from 'gatsby'
 import './src/assets/styles/globals.css'
+import './src/assets/styles/easteregg.css'
 import {TooltipProvider} from './src/components/ui/tooltip'
 
 export const wrapRootElement: GatsbyBrowser['wrapRootElement'] = ({
