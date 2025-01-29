606ffd7075da4956075cfd141c2c0885bc77f957
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2025-01-29 15:52:20 +0530

    feat: web manifest & favicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,21 @@ import tailwind from "@astrojs/tailwind"
 
 import { readingTime } from "./src/plugins/remark/reading-time"
 
-const { URL } = loadEnv(import.meta.env.MODE, process.cwd(), "")
+import favicons from "astro-favicons"
+
+const { URL, APP_NAME, APP_SHORT_NAME } = loadEnv(import.meta.env.MODE, process.cwd(), "")
 
 // https://astro.build/config
 export default defineConfig({
   site: URL,
   integrations: [
     react(),
+    favicons({
+      name: APP_NAME,
+      short_name: APP_SHORT_NAME,
+      pixel_art: true,
+      themes: ["#ffffff", "#020617"],
+    }),
     tailwind({ applyBaseStyles: false }),
     sitemap({
       filter: (page) => !page.includes(`/admin/`) && !page.includes(`/tags/`),
