d55664871e4b12aed43896c152e14cd2fdaff89f
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2025-03-11 15:07:40 +0530

    feat: GA4 using partytown :tada:

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,4 +3,5 @@ interface ImportMetaEnv {
   readonly PUBLIC_GISCUS_REPO_ID: string
   readonly PUBLIC_GISCUS_CATEGORY: string
   readonly PUBLIC_GISCUS_CATEGORY_ID: string
+  readonly PUBLIC_GOOGLE_ANALYTICS_ID: string
 }
