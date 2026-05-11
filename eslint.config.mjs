f26a3623214612c9735acf63994985ce7d5f9a7b
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2026-05-11 18:31:06 +0530

    feat: remove unused packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,8 @@
 import pluginReact from "eslint-plugin-react"
-import tailwind from "eslint-plugin-tailwindcss"
-import eslintPluginAstro from "eslint-plugin-astro"
 import reactCompiler from "eslint-plugin-react-compiler"
 import typescriptParser from "@typescript-eslint/parser"
 
 export default [
-  ...tailwind.configs["flat/recommended"],
-  ...eslintPluginAstro.configs.recommended,
   {
     ...pluginReact.configs.flat.recommended,
     files: ["**/*.{jsx,tsx}"],
